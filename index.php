e9f49bf18a166618d89bcc5f076af5812576d79a
Riina Hemmilä <riinah97@gmail.com>
2023-01-26 00:04:05 +0200

    fixed mobile view and did some reordering of divs

--- index.php
+++ index.php
@@ -15,6 +15,11 @@
     ?>
 
     <div class="bottom">
+
+        <?php
+            require_once "navi.php";
+        ?>
+        
         <div class="desc">
             <h2 class="text_headline">Tervetuloa Ropaz.dev:in pääsivulle!</h2>
             <p class="text">Tämä sivusto toimii niin sanottuna telakkana harrastuksiini liittyen; täältä voit löytää muun muassa linkit kuvauksineen joillekin muille verkkosivuilleni sekä kuvia ja peräti tekstiä joihinkin askarteluprojekteihini liittyen.
@@ -22,10 +27,6 @@
             Tänne sivulle on tarkoitus lisätä myös listamuotoinen kalenteri, joka kattaa aina seuraavan viikon päivät. Kalenteriin voin sitten lisätä, mitä olen ajatellut tehdä kunakin päivänä. Tämä ominaisuus tulee kuitenkin vain minun käyttööni, ja se tulee vaatimaan kirjautumisen.
             </p>
         </div>
-
-        <?php
-            require_once "navi.php";
-        ?>
 
     </div>
 <script type="text/javascript" src="scripts.js"></script>
