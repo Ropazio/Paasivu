24d1714560ec250ede459cc3cd6ee215ffbcecfb
Riina Hemmilä <riinah97@gmail.com>
2023-01-25 14:22:52 +0200

    added some of my handcrafts

--- hobby_projects.php
+++ hobby_projects.php
@@ -15,11 +15,128 @@
     <div class="bottom">
         <div class="desc">
             <h2 class="main_text_headline">Askarteluja</h2>
-            <p class="text">Tänne lisäilen kuvia askarteluistani, joihin lukeutuu paljon virkkaustöitä ja jonkin verran puutöitä sekä satunnaisia muita projekteja.</p>
+            <p class="text">Tänne lisäilen kuvia askarteluistani, joihin lukeutuu paljon virkkaustöitä ja jonkin verran puutöitä sekä satunnaisia muita projekteja. Pahoittelut, että joistain puuttuu vielä kuvat!
+            </p>
 
-            <h3 class="text_headline" id="heips">Heips</h3>
-            <p class="text">Jeppis.
+            <h3 class="text_headline" id="crochet">Virkatut hahmot</h3>
+            <p class="text">Hahmojen virkkaaminen on ollut varsin antoisaa hommaa yksityskohtiensa vuoksi. Tässäpä lista hahmoista kuvineen, mitä olen saanut tuherrettua:
+            <ol class="items">
+                <li>
+                    Geralt of Rivia, The Witcher 1 -pelin mukaan
+                </li>
+                <li>
+                    Geralt of Rivia, The Witcher -sarjan mukaan (En tosin pidä henkilökohtaisesti sarjasta.)
+                </li>
+                <li>
+                    Morrigan, Dragon Age Origins -pelin mukaan
+                </li>
+                <li>
+                    Ruttotohtori
+                </li>
+                <li>
+                    Haku-lohikäärme, Studio Ghiblin Henkien kätkemä -elokuvasta
+                </li>
+                <li>
+                    Totoro, Studio Ghiblin Naapurini Totoro -elokuvasta
+                </li>
+                <li>
+                    "Caveira", Rainbow six siege -pelistä
+                </li>
+                <li>
+                    Hawke, Dragon Age 2 -pelin mukaan (Hahmon vaatteet eivät ole minun tekemiäni tällä kertaa.)
+                </li>
+                <li>
+                    Baby Yoda, The Mandalorian -sarjasta
+                </li>
+                <li>
+                    Extra: Silmät hammasmallin pariksi goottisarjojen innoittamana!
+                </li>
+            </ol>
             <br>&emsp;
+            </p>
+            <img src="projects/Geralt1.jpg" alt="1" class="proj_img">
+            <img src="projects/Geralt2.jpg" alt="2" class="proj_img">
+            <img src="projects/Geralts.jpg" alt="1 ja 2" class="proj_img">
+            <img src="projects/Morrigan.jpg" alt="3" class="proj_img">
+            <img src="projects/Doctor2.jpg" alt="4" class="proj_img"><img src="projects/Doctor1.jpg" alt="4" class="proj_img">
+            <img src="projects/Caveira.jpg" alt="7" class="proj_img">
+            <img src="projects/" alt="8" class="proj_img">
+            <img src="projects/Yoda.jpg" alt="9" class="proj_img"><img src="projects/Yoda2.jpg" alt="9" class="proj_img">
+            <img src="projects/Eyes.jpg" alt="10" class="proj_img">
+            <img src="projects/Totoro.jpg" alt="6" class="proj_img">
+            <img src="projects/Haku.jpg" alt="5" class="proj_img" style="max-width: 600px;">
+
+
+            <h3 class="text_headline" id="textile">Tekstiilityöt</h3>
+            <p class="text">Joitain tekstiilitöitä:
+            <ol class="items" start="11">
+                <li>
+                    Alma
+                </li>
+                <li>
+                    Vauvan joulutossut
+                </li>
+                <li>
+                    Huovutettu kettu
+                </li>
+                <li>
+                    Kerttu
+                </li>
+                <li>
+                    T-paita ja yöhousut
+                </li>
+                <li>
+                    Makramee
+                </li>
+            </ol>
+
+            <img src="projects/Alma.jpeg" alt="11" class="proj_img">
+            <img src="projects/Shoes.jpeg" alt="12" class="proj_img">
+            <img src="projects/Fox.jpg" alt="13" class="proj_img">
+            <img src="projects/Kerttu.jpg" alt="14" class="proj_img">
+            <img src="projects/Makrame.jpg" alt="16" class="proj_img">
+            <img src="projects/Clothes.jpg" alt="15" class="proj_img" style="max-width: 600px;">
+            </p>
+
+
+            <h3 class="text_headline" id="tech">Tekniset työt</h3>
+            <p class="text">Joitain teknisiä töitä:
+            <ol class="items" start="17">
+                <li>
+                    Puukko (Teränä vanhan Moran terä.)
+                </li>
+                <li>
+                    Jouluporo
+                </li>
+            </ol>
+
+            <img src="projects/Puukko1.jpg" alt="17" class="proj_img">
+            <img src="projects/Puukko2.jpg" alt="17" class="proj_img">
+            <img src="projects/Reindeer.jpg" alt="18" class="proj_img">
+            </p>
+
+
+            <h3 class="text_headline" id="other">Muut käsityöt</h3>
+            <p class="text">Muita paskarteluja:
+            <ol class="items" start="19">
+                <li>
+                    Enderman, Minecraft-pelistä
+                </li>
+                <li>
+                    Pörriäinen, Minecraft-pelistä
+                </li>
+                <li>
+                    Lasinpuhallusta: pingviini, lintu, koristepallo, maljakko x 2
+                </li>
+                <li>
+                    Normandy SR2, Mass Effect 2 -pelistä
+                </li>
+            </ol>
+
+            <img src="projects/Enderman.jpg" alt="19" class="proj_img">
+            <img src="projects/Minecraft.jpg" alt="19 ja 20" class="proj_img">
+            <img src="projects/Glassware.jpg" alt="21" class="proj_img">
+            <img src="projects/Normandy.jpg" alt="22" class="proj_img">
             </p>
         </div>
 
--- navi.php
+++ navi.php
@@ -1,35 +1,43 @@
 <div class="navi">
 
     <h1>
-        <a class="main_headline function" href="http://localhost:8080/Paasivu">Etusivu</a>
+        <a class="main_headline" href="http://localhost:8080/Paasivu">Etusivu</a>
     </h1>
 
     <h2>
-        <a class="headlines function" href="http://localhost:8080/Paasivu/net_projects.php">Verkkoprojektit</a>
+        <a class="headlines" href="http://localhost:8080/Paasivu/net_projects.php">Verkkoprojektit</a>
     </h2>
 
     <div class="content pages">
-        <li>
+        <li class="bullet">
             <a class="headlines" href="http://localhost:8080/Paasivu/net_projects.php#pekkaspaivat">Pekkaspäivät</a>
         </li>
 
-        <li>
+        <li class="bullet">
             <a class="headlines" href="http://localhost:8080/Paasivu/net_projects.php#nettikasvio">Nettikasvio</a>
         </li>
     </div>
 
 
     <h2>
-        <a class="headlines function" href="http://localhost:8080/Paasivu/hobby_projects.php">Askarteluprojektit</a>
+        <a class="headlines" href="http://localhost:8080/Paasivu/hobby_projects.php">Askarteluprojektit</a>
     </h2>
 
     <div class="content projects">
-        <li>
-            <a class="headlines">heips</a>
+        <li class="bullet">
+            <a class="headlines" href="http://localhost:8080/Paasivu/hobby_projects.php#crochet">Virkatut hahmot </a>
         </li>
 
-        <li>
-            <a class="headlines">deips</a>
+        <li class="bullet">
+            <a class="headlines" href="http://localhost:8080/Paasivu/hobby_projects.php#textile">Tekstiilityöt</a>
+        </li>
+
+        <li class="bullet">
+            <a class="headlines" href="http://localhost:8080/Paasivu/hobby_projects.php#tech">Tekniset työt</a>
+        </li>
+
+        <li class="bullet">
+            <a class="headlines" href="http://localhost:8080/Paasivu/hobby_projects.php#other">Muut käsityöt</a>
         </li>
     </div>
 
