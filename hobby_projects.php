6d4ebf170f201a6042ff33b57fbdbc770b24463a
Riina Hemmilä <riinah97@gmail.com>
2024-02-08 19:00:46 +0200

    fixed multiple image addition

--- hobby_projects.php
+++ hobby_projects.php
@@ -6,41 +6,41 @@
 array (ordinal number: int, project description: str, image source: str, projectwide image: bool )
 */
 $character_projects = array (
-    array(1, 'Geralt of Rivia, The Witcher 1 -pelin mukaan', 'projects/Geralt1.jpg', 'Geralt of Rivia (#1)', false),
-    array(2, 'Geralt of Rivia, The Witcher -sarjan mukaan (En tosin pidä henkilökohtaisesti sarjasta.)', 'projects/Geralt2.jpg', 'Geralt of Rivia (#2)', false),
-    array(3, 'Morrigan, Dragon Age Origins -pelin mukaan', 'projects/Morrigan.jpg', 'Morrigan', false),
-    array(4, 'Ruttotohtori', 'projects/Doctor1.jpg', 'Ruttotohtori', false),
-    array(5, 'Caveira, Rainbow six siege -pelistä', 'projects/Caveira.jpg', 'Caveira', false),
-    array(6, 'Totoro, Studio Ghiblin Naapurini Totoro -elokuvasta', 'projects/Totoro.jpg', 'Totoro', false),
-    array(7, 'Hawke, Dragon Age 2 -pelin mukaan (Hahmon vaatteet eivät ole minun tekemiäni tällä kertaa.)', 'projects/Hawke.jpg', 'Hawke', false),
-    array(8, 'Baby Yoda, The Mandalorian -sarjasta', 'projects/Yoda.jpg', 'Baby Yoda', false),
-    array(9, 'Silmät hammasmallin pariksi goottisarjojen innoittamana!', 'projects/Eyes.jpg', 'Silmämunat', false),
-    array(10, 'Haku-lohikäärme, Studio Ghiblin Henkien kätkemä -elokuvasta', 'projects/Haku.jpg', 'Haku', true)
+    array(1, 'Geralt of Rivia, The Witcher 1 -pelin mukaan', [['projects/Geralt1.jpg', false, 'Geralt of Rivia (#1)']]),
+    array(2, 'Geralt of Rivia, The Witcher -sarjan mukaan (En tosin pidä henkilökohtaisesti sarjasta.)', [['projects/Geralt2.jpg', false, 'Geralt of Rivia (#2)'], ['projects/Geralts.jpg', false, 'Geralts']]),
+    array(3, 'Morrigan, Dragon Age Origins -pelin mukaan', [['projects/Morrigan.jpg', false, 'Morrigan']]),
+    array(4, 'Ruttotohtori', [['projects/Doctor1.jpg', false, 'Ruttotohtori'], ['projects/Doctor2.jpg', false, 'Ruttotohtori']]),
+    array(5, 'Caveira, Rainbow six siege -pelistä', [['projects/Caveira.jpg', false, 'Caveira']]),
+    array(6, 'Totoro, Studio Ghiblin Naapurini Totoro -elokuvasta', [['projects/Totoro.jpg', false, 'Totoro']]),
+    array(7, 'Hawke, Dragon Age 2 -pelin mukaan (Hahmon vaatteet eivät ole minun tekemiäni tällä kertaa.)', [['projects/Hawke.jpg', false, 'Hawke']]),
+    array(8, 'Baby Yoda, The Mandalorian -sarjasta', [['projects/Yoda.jpg', false, 'Baby Yoda'], ['projects/Yoda2.jpg', false, 'Baby Yoda ilman viittaa']]),
+    array(9, 'Silmät hammasmallin pariksi goottisarjojen innoittamana!', [['projects/Eyes.jpg', false, 'Silmämunat']]),
+    array(10, 'Haku-lohikäärme, Studio Ghiblin Henkien kätkemä -elokuvasta', [['projects/Haku.jpg', true, 'Haku']])
 );
 
 $textile_projects = array (
-    array(1, 'Alma', 'projects/Alma.jpeg', 'Alma', false),
-    array(2, 'Vauvan joulutossut', 'projects/Shoes.jpeg', 'Vauvan joulutossut', false),
-    array(3, 'Huovutettu kettu', 'projects/Fox.jpg', 'Huovutettu kettu', false),
-    array(4, 'Kerttu', 'projects/Kerttu.jpg', 'Kerttu', false),
-    array(5, 'Makramee', 'projects/Makrame.jpg', 'Makramee', false),
-    array(6, 'T-paita ja yöhousut', 'projects/Clothes.jpg', 'T-paita ja yöhousut', true),
-    array(7, 'Paita', 'projects/Shirt.jpg', 'Paita', true)
+    array(1, 'Alma', [['projects/Alma.jpeg', false, 'Alma']]),
+    array(2, 'Vauvan joulutossut', [['projects/Shoes.jpeg', false, 'Vauvan joulutossut']]),
+    array(3, 'Huovutettu kettu', [['projects/Fox.jpg', false, 'Huovutettu kettu']]),
+    array(4, 'Kerttu', [['projects/Kerttu.jpg', false, 'Kerttu']]),
+    array(5, 'Makramee', [['projects/Makrame.jpg', false, 'Makramee']]),
+    array(6, 'T-paita ja yöhousut', [['projects/Clothes.jpg', true, 'T-paita ja yöhousut']]),
+    array(7, 'Paita', [['projects/Shirt.jpg', true, 'Paita']])
 );
 
 $technical_projects = array (
-    array(1, 'Puukko (Teränä vanhan Moran terä.)', 'projects/Puukko1.jpg', 'Puukko', false),
-    array(2, 'Jouluporo', 'projects/Reindeer.jpg', 'Jouluporo', false),
-    array(3, 'Geraltin hopeamiekka', 'projects/Silver_sword.jpg', 'Geraltin hopeamiekka', false),
-    array(4, 'Kasvitaso', 'projects/Plant_stand.jpg', 'Kasvitaso', false)
+    array(1, 'Puukko (Teränä vanhan Moran terä.)', [['projects/Puukko1.jpg', false, 'Puukko'], ['projects/Puukko2.jpg', false, 'Puukko']]),
+    array(2, 'Jouluporo', [['projects/Reindeer.jpg', false, 'Jouluporo']]),
+    array(3, 'Geraltin hopeamiekka', [['projects/Silver_sword.jpg', false, 'Geraltin hopeamiekka']]),
+    array(4, 'Kasvitaso', [['projects/Plant_stand.jpg', false, 'Kasvitaso']])
 );
 
 $other_projects = array (
-    array(1, 'Enderman, Minecraft-pelistä', 'projects/Enderman.jpg', 'Enderman', false),
-    array(2, 'Pörriäinen, Minecraft-pelistä', 'projects/Minecraft.jpg', 'Pörriäinen', false),
-    array(3, 'Lasinpuhallusta: pingviini, lintu, koristepallo, maljakko x 2', 'projects/Glassware.jpg', 'Lasinpuhallustöitä', false),
-    array(4, 'Nuka-Cola Quantum, Fallout-pelissarjasta (Etiketti netistä.)', 'projects/Bottle.jpg', 'Nuka-Cola Quantum', false),
-    array(5, 'Normandy SR-2, Mass Effect 2 -pelistä (Nimi puuttuu aluksen kyljestä. Löysin aluksen ja sukkuloiden 3D-mallit täältä: https://www.thingiverse.com/thing:4921765. Tuki omasta päästä.)', 'projects/Normandy.jpg', 'Normandy SR-2', true)
+    array(1, 'Enderman, Minecraft-pelistä', [['projects/Enderman.jpg', false, 'Enderman']]),
+    array(2, 'Pörriäinen, Minecraft-pelistä', [['projects/Minecraft.jpg', false, 'Pörriäinen']]),
+    array(3, 'Lasinpuhallusta: pingviini, lintu, koristepallo, maljakko x 2', [['projects/Glassware.jpg', false, 'Lasinpuhallustöitä']]),
+    array(4, 'Nuka-Cola Quantum, Fallout-pelissarjasta (Etiketti netistä.)', [['projects/Bottle.jpg', false, 'Nuka-Cola Quantum']]),
+    array(5, 'Normandy SR-2, Mass Effect 2 -pelistä (Nimi puuttuu aluksen kyljestä. Löysin aluksen ja sukkuloiden 3D-mallit täältä: https://www.thingiverse.com/thing:4921765. Tuki omasta päästä.)', [['projects/Normandy.jpg', true, 'Normandy SR-2']])
 );
 
 
@@ -51,19 +51,23 @@ function add_project_in_list($projects, $project_ordinal) {
 }
 
 function add_project_image($projects, $project_ordinal) {
-    if ($projects[$project_ordinal][4] == false) {
-        $name_class_container = 'image_container';
-        $name_class_image = 'proj_img';
-    } else {
-        $name_class_container = 'wide_image_container';
-        $name_class_image = 'wide_img';
+    $image_list = $projects[$project_ordinal][2];
+    for ($i = 0; $i < count($image_list); $i++) {
+        if ($image_list[$i][1] == false) {
+            $name_class_container = 'image_container';
+            $name_class_image = 'proj_img';
+        } else {
+            $name_class_container = 'wide_image_container';
+            $name_class_image = 'wide_img';
+        }
+
+        echo "<div class=" . $name_class_container . ">";
+        echo    "<img src=" . $image_list[$i][0]
+                . " class=" . $name_class_image
+                . " onclick='enlarge_image(" . $image_list[$i][0] . ")'>";
+        echo    "<div class='overlay'>" . $image_list[$i][2] . "</div>";
+        echo "</div>";
     }
-    echo "<div class=" . $name_class_container . ">";
-    echo    "<img src=" . $projects[$project_ordinal][2] 
-            . " class=" . $name_class_image
-            . " onclick='enlarge_image(" . $projects[$project_ordinal][2] . ")'>";
-    echo    "<div class='overlay'>" . $projects[$project_ordinal][3] . "</div>";
-    echo "</div>";
 }
 
 ?>
@@ -109,18 +113,6 @@ function add_project_image($projects, $project_ordinal) {
                         add_project_image($character_projects, $i);
                     }
                 ?>
-                <div class="image_container">
-                    <img src="projects/Geralts.jpg" alt="1 ja 2" class="proj_img" id="3">
-                    <div class="overlay">Geralttien yhteiskuva</div>
-                </div>
-                <div class="image_container">
-                    <img src="projects/Doctor2.jpg" alt="4" class="proj_img">
-                    <div class="overlay">Ruttotohtori</div>
-                </div>
-                <div class="image_container">
-                    <img src="projects/Yoda2.jpg" alt="9" class="proj_img">
-                    <div class="overlay">Baby Yoda ilman viittaa</div>
-                </div>
             </div>
 
 
@@ -165,10 +157,6 @@ function add_project_image($projects, $project_ordinal) {
                         add_project_image($technical_projects, $i);
                     }
                 ?>
-                <div class="image_container">
-                    <img src="projects/Puukko2.jpg" alt="17" class="proj_img">
-                    <div class="overlay">Puukko</div>
-                </div>
             </div>
             
 
