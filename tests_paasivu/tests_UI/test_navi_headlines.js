1b7f65e6b4da846beacb868c32ef4f5ec444893b
Riina Hemmilä <riinah97@gmail.com>
2024-02-28 17:32:17 +0200

    Added tests for subheadlines in the navi as well. Also created general function for the tests.

--- tests_paasivu/tests_UI/test_navi_headlines.js
+++ tests_paasivu/tests_UI/test_navi_headlines.js
@@ -5,6 +5,20 @@ const CHROME_EXE_PATH = '/Users/riina/Documents/Ohjelmointi/Selenium_testing/web
 
 describe('Navi headline links', () => {
   let driver;
+  let headline;
+  let linkAddress;
+
+  var testLink = async (headline, linkAddress) => {
+    try {
+      await driver.findElement(By.linkText(headline)).click();
+      let actualUrl = await driver.getCurrentUrl();
+      let expectedUrl = linkAddress;
+      assert.equal(expectedUrl, actualUrl);
+    } catch(error) {
+      throw error;
+    }
+  }
+
 
   beforeEach(async () => {
     // use Chrome
@@ -16,53 +30,25 @@ describe('Navi headline links', () => {
 
   describe('Try pressing "Etusivu"', () => {
     it('should go to "Etusivu"', async () => {
-      try {
-        await driver.findElement(By.linkText('Etusivu')).click();
-        let actualUrl = await driver.getCurrentUrl();
-        let expectedUrl = 'http://localhost:8080/Paasivu/public_html/pages/front_page.php';
-        assert.equal(expectedUrl, actualUrl);
-      } catch(error) {
-        throw error;
-      }
+      testLink('Etusivu', 'http://localhost:8080/Paasivu/public_html/pages/front_page.php');
     });
   });
 
   describe('Try pressing "Verkkoprojektit"', () => {
     it('should go to "Verkkoprojektit"', async () => {
-      try {
-        await driver.findElement(By.linkText('Verkkoprojektit')).click();
-        let actualUrl = await driver.getCurrentUrl();
-        let expectedUrl = 'http://localhost:8080/Paasivu/public_html/pages/net_projects.php';
-        assert.equal(expectedUrl, actualUrl);
-      } catch(error) {
-        throw error;
-      }
+      testLink('Verkkoprojektit', 'http://localhost:8080/Paasivu/public_html/pages/net_projects.php');
     });
   });
 
   describe('Try pressing "Askarteluprojektit"', () => {
     it('should go to "Askarteluprojektit"', async () => {
-      try {
-        await driver.findElement(By.linkText('Askarteluprojektit')).click();
-        let actualUrl = await driver.getCurrentUrl();
-        let expectedUrl = 'http://localhost:8080/Paasivu/public_html/pages/hobby_projects.php';
-        assert.equal(expectedUrl, actualUrl);
-      } catch(error) {
-        throw error;
-      }
+      testLink('Askarteluprojektit', 'http://localhost:8080/Paasivu/public_html/pages/hobby_projects.php');
     });
   });
 
   describe('Try pressing "Elämänhallinta" without being signed in', () => {
     it('should go to login page', async () => {
-      try {
-        await driver.findElement(By.linkText('Elämänhallinta')).click();
-        let actualUrl = await driver.getCurrentUrl();
-        let expectedUrl = 'http://localhost:8080/Paasivu/public_html/pages/login_page.php';
-        assert.equal(expectedUrl, actualUrl);
-      } catch(error) {
-        throw error;
-      }
+      testLink('Elämänhallinta', 'http://localhost:8080/Paasivu/public_html/pages/login_page.php');
     });
   });
 
@@ -73,13 +59,10 @@ describe('Navi headline links', () => {
         await driver.findElement(By.name('username')).sendKeys('correct username');
         await driver.findElement(By.name('password')).sendKeys('correct password', Key.RETURN);
         await driver.findElement(By.linkText('Etusivu')).click();
-        await driver.findElement(By.linkText('Elämänhallinta')).click();
-        let actualUrl = await driver.getCurrentUrl();
-        let expectedUrl = 'http://localhost:8080/Paasivu/public_html/pages/calendar.php';
-        assert.equal(expectedUrl, actualUrl);
       } catch(error) {
         throw error;
       }
+      testLink('Elämänhallinta', 'http://localhost:8080/Paasivu/public_html/pages/calendar.php');
     });
   });
 
